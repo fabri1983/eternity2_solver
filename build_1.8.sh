98949d81b4234ff79b5bad866f3d6a102f9b7cfd
pablo.lettieri <pablo.lettieri@etermax.com>
2015-07-15 10:45:56 -0300

    Merged with dev

---
+++ build_1.8.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo Compilando...
+
+# lib para envio de emails. Java 1.6 ya tiene una. Usar esa.
+# -cp ./extern_libs/jaf-1.1/activation.jar
+
+javac -source 1.8 -target 1.8 -O -g:none -cp ./extern_libs/javamail-1.4.5/mail.jar -sourcepath ./eternity_faster_pkg eternity_faster_pkg/*.java eternity_faster_pkg_arrays/*.java graphic/EternityCanvas.java graphic/EternityCellRenderer.java graphic/EternityII.java graphic/EternityModel.java graphic/EternityTable.java graphic/ViewEternity.java
+
+jar cvfe E2Faster.jar eternity_faster_pkg.MainFaster eternity_faster_pkg/*.class eternity_faster_pkg_arrays/*.class graphic/*.class graphic/image/*.png
