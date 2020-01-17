fec070e414fd22b77be613d90219494078d54834
Fabricio Lettieri <fabri1983@gmail.com>
2020-01-17 16:22:51 -0300

    Additional experimental tests for reducing PHASHRANGE

--- src/test/java/org/fabri1983/eternity2/core/mph/PerfectHashFunctionTest.java
+++ src/test/java/org/fabri1983/eternity2/core/mph/PerfectHashFunctionTest.java
@@ -1,6 +1,9 @@
 package org.fabri1983.eternity2.core.mph;
 
+import java.util.stream.IntStream;
+
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class PerfectHashFunctionTest {
@@ -9,9 +12,12 @@ public class PerfectHashFunctionTest {
 
 	@Test
 	public void testIndexUniqueness() {
+		boolean doPrint = false;
 		for (int key : loadSuperMatrizKeys()) {
-			int index = PerfectHashFunction.hash(key);
-//			System.out.println(String.format("%s %s", padRight(index+"", 8), key));
+			int index = PerfectHashFunction.phash(key);
+			if (doPrint) {
+				System.out.println(String.format("%s %s", padRight(index+"", 8), key));
+			}
 			if (super_matriz[index] != 0) {
 				Assert.fail(String.format("Duplicated index %s for key %s", index, key));
 				System.exit(0);
@@ -21,18 +27,120 @@ public class PerfectHashFunctionTest {
 		}
 	}
 	
-//	private static String padRight(String inputString, int length) {
-//	    if (inputString.length() >= length) {
-//	        return inputString;
-//	    }
-//	    StringBuilder sb = new StringBuilder(inputString.length() + length);
-//	    sb.append(inputString);
-//	    while (sb.length() < length) {
-//	        sb.append(' ');
-//	    }
-//	 
-//	    return sb.toString();
-//	}
+	@Test
+	public void testDeflateInflateTab() {
+		// Since integers x in PerfectHashFunction.tab are in the range [0, 2^16] then we can pack 2 of them into 1 int (2^32).
+		// We consider the fact that Java uses last high order bit as the bit sign so when inflating use >>> operand.
+		
+		// generate deflated array
+		int[] tabDeflated = new int[PerfectHashFunction.tab.length/2]; // tab[].length is ensured to be power of 2
+		for (int i=0; i < tabDeflated.length; ++i) {
+			int high = PerfectHashFunction.tab[(2*i)];
+			int low = PerfectHashFunction.tab[(2*i) + 1];
+			tabDeflated[i] = high << 16 | low;
+		}
+		
+		boolean doPrint = false;
+		if (doPrint) {
+			IntStream.of(PerfectHashFunction.tab)
+				.boxed()
+				.forEach( n -> System.out.println(n + ","));
+		}
+		
+		// evaluate inflated array
+		for (int i=0; i < tabDeflated.length; ++i) {
+			int high = PerfectHashFunction.tab[(2*i)];
+			int low = PerfectHashFunction.tab[(2*i) + 1];
+			int deflated = tabDeflated[i];
+			int highInflated = deflated >>> 16;
+			int lowInflated = deflated & 0xffff;
+			Assert.assertEquals("high numbers don't match.", high, highInflated);
+			Assert.assertEquals("low numbers don't match.", low, lowInflated);
+		}
+	}
+	
+	@Test
+	@Ignore
+	public void testDivisionReduction() {
+		// 1/93 = 0.01075268817204301075268817204301
+		// 1/93 is roughly 705/65536 = 705/2^16
+		// 705 comes from 65536/93 = 2^16/93 = 704.69
+		//
+		// Then find power of 2 numbers that add up to 704/65536:
+		//   512/65536 + 128/65536 + 64/65536 = 704/65536
+		// (Note here that 704/65536 = 0.0107421875 (error appears at 4th decimal position in comparison with 1/93))
+		//
+		// This leave us with next formula:
+		//  x * 1/93 ~= x * ( 512/65536 + 128/65536 + 64/65536 )
+		//
+		// Then reduce the fractions since dividend and divisor are power of 2:
+		//     1/128   +   1/512   +  1/1024‬
+		//
+		// There you have your shifting values, leaving us with the next formula:
+		//    (x >> 7) +  (x >> 9) + (x >> 10) ~= x/93
+		//
+		// Then you can add more shifts to reduce more the error:
+		//  (x >> 18) comes from 7 + 9
+		//  (x >> 17) comes from 7 + 10
+		// These last shifts don't add more precision if log2(x) < 17.
+		//
+		// Ending with the formula:
+		//    (x >> 7) +  (x >> 9) + (x >> 10) + (x >> 17) + (x >> 18)
+		//
+		// If you maintain that remainder at each step, the error is eliminated:
+		// (note: now using << instead of >>)
+		//    ((x << 11) + (x << 9) + (x << 8) + (x << 1) + x) >> 18
+		// 
+		// There are some limitations: since we start by shifting to the left by 11 bits, the operand must be constrained. 
+		// If we employ 32-bit arithmetic, the operand would be limited to 21 bits. Not a problem if you're working with 16-bit operands. 
+		// And of course, the need to work with 32-bit intermediate results will have a speed penalty over working with 16 bits.
+		// 
+		// Optimized formula:
+		//    (x << 1) + x
+		//    and
+		//    (x << 9) + (x << 8)
+		//  are the same number, just shifted one byte, witch is almost free.
+		//  So formula ends up being:
+		//    temp = (x << 1) + x;
+		//    result = ((x << 11) + (temp << 8) + temp) >> 18;
+		
+		
+		for (int x = 93; x <= 16121; ++x) {
+			int expected = x/93;
+			int temp = (x << 1) + x;
+			int actual = ((x << 11) + (temp << 8) + temp) >> 18;
+			Assert.assertEquals(String.format("Values don't match for x=%s.", x), expected, actual);
+		}
+	}
+	
+	@Test
+	public void testMinAndMaxIntegers() {
+		int min = IntStream.of(PerfectHashFunction.tab)
+			.boxed()
+			.filter( i -> i != 0 )
+			.min( Integer::compare )
+			.orElseThrow();
+		System.out.println(String.format("Min number in tab[] (not zero): %s", min));
+		
+		int max = IntStream.of(PerfectHashFunction.tab)
+			.boxed()
+			.max( Integer::compare )
+			.orElseThrow();
+		System.out.println(String.format("Max number in tab[]: %s", max));
+	}
+	
+	private static String padRight(String inputString, int length) {
+	    if (inputString.length() >= length) {
+	        return inputString;
+	    }
+	    StringBuilder sb = new StringBuilder(inputString.length() + length);
+	    sb.append(inputString);
+	    while (sb.length() < length) {
+	        sb.append(' ');
+	    }
+	 
+	    return sb.toString();
+	}
 	
 	private static int[] loadSuperMatrizKeys() {
 		// array length is 6954
